a8b76c5a7304ba169838a20384ab7c4739c32eb8
Jade Miller <mil19017@byui.edu>
2023-10-14 21:49:39 -0600

    up to 3

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,14 +1,17 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-
+const templesElement = document.getElementById('temples');
+let templeList = [];
 
 /* async displayTemples Function */
-
-
-
+const displayTemples = (temples) => {
+}
 
 /* async getTemples Function using fetch()*/
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+}
 
 
 /* reset Function */
