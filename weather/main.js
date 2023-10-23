db4b9d1e6eeae92b0bcf5498665973166b9c0607
Jade Miller <mil19017@byui.edu>
2023-10-23 13:50:14 -0600

    css and html

--- weather/main.js
+++ weather/main.js
@@ -1,2 +1,10 @@
-//At least one module is used with supporting import and export functionality. EXAMPLE NOT PART OF ANYTHING
-import {get_data, anotherimaginaryfunctionthatdoesnotexistrightnow} from "./functions";
+// import {} from "./functions";
+
+const regions = {
+    Northeast: ["Connecticut", "Delaware", "Maine", "Maryland", "Massachusetts", "New Hampshire", "New Jersey", "New York", "Pennsylvania", "Rhode Island", "Vermont"],
+    Southeast: ["Alabama", "Arkansas", "Florida", "Georgia", "Kentucky", "Louisiana", "Mississippi", "North Carolina", "South Carolina", "Tennessee", "Virginia", "West Virginia"],
+    West: ["Alaska", "Arizona", "California", "Colorado", "Hawaii", "Idaho", "Montana", "Nevada", "New Mexico", "Oregon", "Utah", "Washington", "Wyoming"],
+    Midwest: ["Illinois", "Indiana", "Iowa", "Kansas", "Michigan", "Minnesota", "Missouri", "Nebraska", "North Dakota", "Ohio", "South Dakota", "Wisconsin"]
+};
+
+export default {regions};
