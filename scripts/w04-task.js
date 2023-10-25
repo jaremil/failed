a31218fd4a730453d2c84ae188b409f965d078e1
Jade Miller <mil19017@byui.edu>
2023-10-24 23:38:27 -0600

    commit

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,25 +1,53 @@
 /* LESSON 3 - Programming Tasks */
 
-/* Profile Object  */
+
+let myProfile = {};
+
+myProfile.name = "Jade Miller";
+myProfile.photo = "images/profile.jpeg"; 
+myProfile.favoriteFoods = [
+  'Gyro',
+  'Sushi',
+  'Cake',
+];
+myProfile.hobbies = ['Art', 'Cooking', 'Going on walks']; 
+myProfile.placesLived = [];
 
 
-/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+  {
+    place: 'Idaho',
+    length: '21 years'
+  },
+  {
+    place: 'Chicago',
+    length: '1 year'
+  }
+);
 
 
-/* DOM Manipulation - Output */
+document.querySelector('#name').textContent = myProfile.name;
+document.querySelector('#photo').src = myProfile.photo;
+document.querySelector('#photo').alt = myProfile.name;
 
 
-/* Name */
+myProfile.favoriteFoods.forEach(food => {
+  let li = document.createElement('li');
+  li.textContent = food;
+  document.querySelector('#favorite-foods').appendChild(li);
+});
 
+myProfile.hobbies.forEach(hobby => {
+  let li = document.createElement('li');
+  li.textContent = hobby;
+  document.querySelector('#hobbies').appendChild(li);
+});
 
-/* Photo with attributes */
-
-
-/* Favorite Foods List*/
-
-
-/* Hobbies List */
-
-
-/* Places Lived DataList */
-
+myProfile.placesLived.forEach(place => {
+  let dt = document.createElement('dt');
+  dt.textContent = place.place;
+  let dd = document.createElement('dd');
+  dd.textContent = place.length;
+  document.querySelector('#places-lived').appendChild(dt);
+  document.querySelector('#places-lived').appendChild(dd);
+});
